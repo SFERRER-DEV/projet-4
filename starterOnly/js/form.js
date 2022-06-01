8d7e96044ff637d832f3a7d261db258007249c40
Stef <3736753+SFERRER-DEV@users.noreply.github.com>
2022-06-01 11:28:36 +0200

     Refactoring de la fonction validateField(field) pour les types date, number et checkbox

--- starterOnly/js/form.js
+++ starterOnly/js/form.js
@@ -11,24 +11,20 @@ export function checkValidity(e) {
     // Flag de test pour les contraintes de validation
     let valid = true;    
     // Récupérer un premier ensemble d'élements du formulaire à valider
-    const fields = document.querySelectorAll('.formData input[type="text"], input[type="email"], input[type="date"], input[type="number"]');
+    const fields = document.querySelectorAll('.formData input[type="text"], input[type="email"], input[type="date"], input[type="number"], input[type="checkbox"]');
     // Récupérer les éléments radio pour les emplacements des tournois
     const radios = document.getElementsByName('location');
-    // Récupérer la case à cocher d'acceptation des conditions
-    const accept = document.querySelector('#checkbox1');
 
     // Parcourir les champs input du formulaire
     for(let input of fields){
         // Vérifier si les contraintes de chaque élément sont valides et afficher les messages de correction sur le navigateur.
         switch(input.type)
         {
-            case "date":
-                valid &= func.validateBirth(input)
-                break;
-
             case "text":
             case "email":
             case "number":
+            case "date":
+            case "checkbox":
             default:
                 valid &= func.validateField(input);
         }
@@ -42,7 +38,7 @@ export function checkValidity(e) {
     // Si la validation est déjà en échec alors les autres champs ne seront pas testés
     if(valid){
         // Si les une fonction n'est pas valide alors arrêter la validation et sinon continuer pour valider la suivante
-        valid &= (func.validateRadio(radios) && func.validateCheck(accept));
+        valid &= func.validateRadio(radios);
 
         // Vérifier que l'intégralité des champs sont valides
         if(valid){
--- starterOnly/js/functions.js
+++ starterOnly/js/functions.js
@@ -4,13 +4,14 @@ import * as util from "./util.js";
  * Fonctions spécifiques dédiées à valider des champs de formulaire
  * suivant le type du champ.
  * - validateField(field)
- * - validateBirth(field)
  * - validateRadio(radios)
- * - validateCheck(check)
  */
 /*
     Fonction dédiée pour valider les champs des types 
-    text, email, number et date
+    text, email, number, date et checkbox
+    Des messages d'erreurs différents sont affichés :
+    - Un message spécifique exsitant dans le tableau des messages pour le nom de ce champ
+    - Ou un des messages génériques depuis l'API Validation
 */
 export const validateField = (field) => {
     const validityState = field.validity;
@@ -19,95 +20,49 @@ export const validateField = (field) => {
     // Rendre valide la présentation container formData
     util.resetValidation(field);
 
-    if (!validityState.valid) { 
+    if ((field.type === 'date' | field.type === 'number') && (validityState.rangeUnderflow | validityState.rangeOverflow)) {
+      // La valeur dépasse les bornes min et max pour les dates et les nombres, utiliser un message générique de l'API Validation
+      message =  field.validationMessage;
+    } else if (!validityState.valid) { 
       // Une contrainte de validation du champ est en échec, préparer son message spécifique d'erreur
       message = mess.messages[field.name];
+    }
+
+    if (message !== '') 
+    {
       // Marquer le container formData en erreur
       util.updateMessageValidation(field, message)
       return false;
+    } else {
+      return true;
     }
-
-    return true;
   }
 
- /*
-    Fonction dédiée pour valider la date de naissance
-    Des messages d'erreurs différents sont affichés :
-    - un message depuis le tableau des messages spécifiques au formulaire
-    - des mesassges génériques depuis l'API Validation
- */
- export const validateBirth = (field) => {
-    const validityState = field.validity;
-    let message = '';
-
-    // Rendre valide le message et la présentation du champ 
-    util.resetValidation(field);
-
-    if (validityState.rangeOverflow | validityState.rangeUnderflow ) {
-    // La date dépasse les bornes min et max, utiliser le message générique de l'API Validation
-        message =  field.validationMessage;
-    } 
-    else if (!validityState.valid) { 
-    // Une contrainte de validation du champ est en échec, préparer son message spécifique d'erreur
-        message = mess.messages[field.name];
-    }
-
-    if (message !== '') { 
-        // Marquer le conteneur formData en erreur
-        util.updateMessageValidation(field, message)
-        return false;
-    }
-
-    return true;
-}
-
 /*
-    Fonction dédiée pour valider les champs du type
-    radio des emplacements de tournois : un radio doit être sélectionné
+    Fonction dédiée pour valider les champs du type radio 
+    Un radio doit être sélectionné pour valider la fonction
 */
 export const validateRadio = (radios) => {
+    let message = '';
     // Obtenir un tableau avec un seul élément contenant la valeur du radio choisi
-    const locationValue = Array.from(radios)
+    const selection = Array.from(radios)
                        .filter(r => r.checked)
                        .map(r => r.value);
+    // Récupérer le premier radio de la collection 
+    const field = radios[0];
     // Tester l'existence de la valeur sélectionnée
-    if (locationValue !== undefined && locationValue.length > 0 && locationValue[0] !== '') 
+    if (selection !== undefined && selection.length > 0 && selection[0] !== '') 
     {
-        // Récupérer le premier radio du groupe: un radio a été séléctionné par l'utilisateur
-        const field = radios[0];
         // Rendre valide la présentation container formData
         util.resetValidation(field);
         return true;
     }
     else
     {
-      // Récupérer le premier radio du groupe: aucun radio n'est sélectionné par l'utilisateur,
-      const field = radios[0];
-      let message = mess.messages[field.name];
+      // Obtenir le  message spécifique d'erreur
+      message = mess.messages[field.name];
       // Marquer le container formData en erreur
       util.updateMessageValidation(field, message)
       return false;
     }
   }
-
-  /*
-    Fonction dédiée pour valider le champ du type 
-    checkbox pour les conditions
-  */
- export const validateCheck =(field) => {
-    let message = '';
-     if(field.checked === true)
-     {
-         // Rendre valide la présentation container formData
-        util.resetValidation(field);
-        return true;
-     }
-     else 
-     {
-        // La case n'est pas cochées, préparer son message spécifique d'erreur
-        message = mess.messages[field.name];
-        // Marquer le container formData en erreur
-        util.updateMessageValidation(field, message)
-        return false;
-     }
- }
